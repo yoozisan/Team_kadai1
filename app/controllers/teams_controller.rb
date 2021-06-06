9bebd03a7c5f4028bd1394830931eb90a0ebf756
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-06 21:19:11 +0900

    オーナー権限移動移動機能実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy change_owner]
 
   def index
     @teams = Team.all
@@ -37,6 +37,15 @@ class TeamsController < ApplicationController
       render :edit
     end
   end
+
+  def change_owner
+      if @team.update(owner_id: params[:owner_id])
+        redirect_to @team, notice: I18n.t('views.messages.change_owner')
+      else
+        flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+        render @team
+      end
+    end
 
   def destroy
     @team.destroy
